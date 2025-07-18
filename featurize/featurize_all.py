9cedc91536c68ac709a5e65324d1f5086c9da3bb
yyoshikai <yyoshikai12@gmail.com>
2025-07-18 17:01:54 +0900

    featurize_all

---
+++ featurize/featurize_all.py
@@ -0,0 +1,47 @@
+import sys, os
+from argparse import ArgumentParser
+from logging import getLogger
+import torch
+from torchvision import transforms as T
+from glob import glob
+WORKDIR = os.environ.get('WORKDIR', "/workspace")
+sys.path += [f"{WORKDIR}/mtpc"]
+from src.featurize import featurize_mtpc
+from src.model.backbone import get_backbone
+from src.utils import set_random_seed
+getLogger('tifffile').disabled = True
+
+parser = ArgumentParser()
+parser.add_argument('--num-workers', type=int, default=1)
+parser.add_argument('--batch-size', type=int, default=512)
+parser.add_argument('--tqdm', action='store_true')
+args = parser.parse_args()
+
+backbone = get_backbone('resnet50')
+transform = T.Compose([
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]), 
+])
+
+for seed in range(5):
+    for path in sorted(glob(f"{WORKDIR}/mtpc/pretrain/results/250715_main/{seed}/**/resnet50.pth", recursive=True)):
+        name = path.removeprefix(f"{WORKDIR}/mtpc/pretrain/results/") \
+                .removesuffix("/resnet50.pth")
+
+        out_dir = f"{WORKDIR}/mtpc/featurize/{name}"
+        if os.path.exists(f"{out_dir}/feat_all.npy") and os.path.exists(f"{out_dir}/feat_added.npy"):
+            print(f"Already finished: {name}")
+            continue
+
+        model_path = f"{WORKDIR}/mtpc/pretrain/results/{name}/resnet50.pth"
+        if not os.path.exists(model_path):
+            print(f"Model not found: {name}")
+            continue
+
+        print(f"Featurizing {name} ...")
+        backbone.load_state_dict(torch.load(model_path, weights_only=True))
+        set_random_seed(seed)
+        featurize_mtpc(name, args.num_workers, args.batch_size, backbone, transform, args.tqdm)
